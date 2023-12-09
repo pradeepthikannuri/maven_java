186d246e4cd740f2f19ec643724b958fd92f7614
pradeepthikannuri <sharadakannuri@gmail.com>
2023-12-09 15:36:16 +0530

    first commit

---
+++ src/main/java/com/pradeepthi/samplejava/App.java
@@ -0,0 +1,13 @@
+package com.pradeepthi.samplejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
